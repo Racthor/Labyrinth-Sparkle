346ae7505d72815a52ff0cf1fe94e1dc4dde5993
Sylvain <kauffeisensylvain@gmail.com>
2018-01-27 22:37:55 +0100

    interrupteurs double + serrure

---
+++ Labyrinth Sparkle/Assets/BtnPourssoirDeuxPerso.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnPourssoirDeuxPerso : MonoBehaviour {
+
+    private int nbRobotsSurPoussoir = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public bool isDeuxRobots() {
+        return (nbRobotsSurPoussoir >= 2) ? true : false;
+    }
+
+    private void OnTriggerEnter( Collider other ) {
+        nbRobotsSurPoussoir += 1;
+    }
+
+    private void OnTriggerExit( Collider other ) {
+        nbRobotsSurPoussoir -= 1;
+
+    }
+}
---
+++ Labyrinth Sparkle/Assets/Interrupteur_double.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interrupteur_double : MonoBehaviour {
+    public GameObject porte;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        porte.GetComponent<Collider>().isTrigger = (GetComponentInChildren<BtnPourssoirDeuxPerso>().isDeuxRobots()) ? true : false;
+    }
+
+    
+}
