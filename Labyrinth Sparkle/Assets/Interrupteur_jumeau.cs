cc14244ef2284fc48067534f81c7aec8337ae442
Sylvain <kauffeisensylvain@gmail.com>
2018-01-27 22:11:26 +0100

    interrupterus jumeaux

---
+++ Labyrinth Sparkle/Assets/Interrupteur_jumeau.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interrupteur_jumeau : MonoBehaviour {
+    public GameObject interrupteur;
+    public GameObject porte;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        porte.GetComponent<Collider>().isTrigger = (GetComponentInChildren<Levier>().isOpen() && interrupteur.GetComponentInChildren<Levier>()) ? true : false;
+
+    }
+}
